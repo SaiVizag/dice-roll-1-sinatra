dd524d738d0644d0553d65053bc68c18245037aa
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2023-07-06 18:00:27 +0000

    Adds automatic code reloading

--- dice.rb
+++ dice.rb
@@ -1,9 +1,14 @@
 require "sinatra"
+require "sinatra/reloader"
 
-get "/" do
+get("/") do
   "Hello World"
 end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
